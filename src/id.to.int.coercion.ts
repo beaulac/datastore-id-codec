25224c9981eacb580581313147550da074dab009
Antoine Beauvais-Lacasse <antoine.beaulac@gmail.com>
2017-12-31 17:58:16 -0500

    Add tests and coverage

--- src/id.to.int.coercion.ts
+++ src/id.to.int.coercion.ts
@@ -2,7 +2,6 @@ import { DatastoreInt, PathElement } from '@google-cloud/datastore/entity';
 import * as debug from 'debug';
 import { DsIdCoercer } from './DsIdCoercer';
 
-const _DEBUG = debug('datastore-id-codec');
 
 export const intCoercer: DsIdCoercer<number> = { coerce };
 export default intCoercer;
@@ -15,14 +14,10 @@ function coerce(datastoreId: PathElement = ''): number {
 
     datastoreId = coerceDsIntToIntStr(datastoreId);
     if (isValidIdString(datastoreId)) {
-        const result = parseInt(coerceDsIntToIntStr(datastoreId), 10);
-        if (isValidNumericId(result)) {
-            return result;
-        }
+        return parseInt(datastoreId, 10);
     }
 
-    _DEBUG(`Invalid DS ID: ${datastoreId}`);
-    throw Error('Invalid Datastore ID');
+    return _invalidId(datastoreId);
 }
 
 function isValidIdString(intstr: PathElement): intstr is string {
@@ -33,8 +28,19 @@ function isValidNumericId(id: PathElement): id is number {
     return (id > 0) && Number.isSafeInteger(id as number);
 }
 
-function coerceDsIntToIntStr(datastoreInt: DatastoreInt | string = ''): string {
-    return typeof datastoreInt === 'string'
-        ? datastoreInt
-        : datastoreInt.value;
+function coerceDsIntToIntStr(datastoreInt: DatastoreInt | string): string {
+    if (typeof datastoreInt === 'string') {
+        return datastoreInt;
+    }
+    const { value } = datastoreInt;
+    if (typeof value === 'string') {
+        return value;
+    }
+
+    return _invalidId(datastoreInt);
+}
+
+function _invalidId(datastoreInt: any): never {
+    debug('datastore-id-codec')(`Invalid DS ID: ${datastoreInt}`);
+    throw Error('Invalid Datastore ID');
 }
---
+++ test/codec.test.ts
@@ -0,0 +1,96 @@
+import * as assert from 'assert';
+import codec from '../src';
+
+function validRepresentationsOf(int: number) {
+    return [int, `${int}`, { value: `${int}` }];
+}
+
+describe('base32 DS ID codec', function () {
+
+    it('passes sanity check', function () {
+
+        const exponent = Math.ceil(Math.random() * 10) + 21;
+        const value = Math.floor(Math.random() * 10000) * Math.pow(2, exponent);
+
+        validRepresentationsOf(value).forEach(
+            v => assert.strictEqual(codec.decode(codec.encode(v)), value)
+        );
+    });
+
+    describe(' encoding ', function () {
+        it('encodes correct values', function () {
+            const fixtures: any[] = require('./encoding.fixtures');
+
+            fixtures.forEach(
+                ([input, encoded]) => {
+                    assert.strictEqual(codec.encode(input), encoded);
+                }
+            );
+        });
+
+        describe(' given invalid input ', function () {
+            it('throws error for undefined value', function () {
+                try {
+                    codec.encode(undefined as any);
+                    assert.fail('should fail');
+                } catch (e) {
+                    assert.strictEqual(e.message, 'Invalid Datastore ID');
+                }
+            });
+
+            it('throws error for too large numbers', function () {
+                try {
+                    codec.encode(9999999999999999);
+                    assert.fail('should fail');
+                } catch (e) {
+                    assert.strictEqual(e.message, 'Invalid Datastore ID');
+                }
+            });
+            
+            it('throws error for odd value', function () {
+                try {
+                    codec.encode(33333333333);
+                    assert.fail('should fail');
+                } catch (e) {
+                    assert.strictEqual(e.message, 'DS ID is not divisible enough');
+                }
+            });
+        });
+    });
+
+    describe(' decoding ', function () {
+        it('decodes correct values', function () {
+            const fixtures: any[] = require('./decoding.fixtures');
+
+            fixtures.forEach(
+                ([encoded, decoded]) => {
+                    assert.strictEqual(codec.decode(encoded), decoded);
+                }
+            );
+        });
+
+        describe(' given invalid input ', function () {
+            it('throws error for undefined value', function () {
+                const invalidId = undefined as any;
+                try {
+                    codec.decode(invalidId);
+                    assert.fail('should fail');
+                } catch (e) {
+                    assert.strictEqual(e.message, `Invalid encoded DS ID: ${invalidId}`);
+                }
+            });
+
+            it('throws error for invalid base32', function () {
+                const invalidId = 'x01';
+                try {
+                    codec.decode(invalidId);
+                    assert.fail('should fail');
+                } catch (e) {
+                    assert.strictEqual(e.message, `Invalid encoded DS ID: ${invalidId}`);
+                }
+            });
+        });
+
+    });
+
+});
